7a101428da201a5de125655eb57784710809f478
youngjae-avikus <youngjae.you@avikus.ai>
2023-10-04 15:23:13 +0900

    Add docker

---
+++ docker/docker_run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+xhost +
+
+docker run \
+	-it \
+	--rm \
+	--gpus all \
+	--net=host \
+	--shm-size=8g \
+	-e DISPLAY=$DISPLAY \
+	-v /tmp/argus_socket:/tmp/argus_socket \
+	-v /tmp/.X11-unix/:/tmp/.X11-unix \
+	-v $PWD:/workspace \
+	-w /workspace \
+	--name ds62-container \
+	fcam_od
